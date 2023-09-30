d3d1887e3c2498511a31762455e7f48fd87241ca
overweightbarracuda <46425687+overweightbarracuda@users.noreply.github.com>
2023-09-30 10:07:37 -0400

    Added common permutations function and commented out cleaning

--- filter.py
+++ filter.py
@@ -2,6 +2,7 @@
 import re, glob
 from tqdm import tqdm
 import gzip
+from itertools import permutations
 
 def save_text_as_gzip(text, filename):
     with gzip.open(filename, 'wt', encoding='utf-8') as file:
@@ -17,24 +18,26 @@ t = remove_latin("as dfn 128 _\n91():!!-=-%")
 print(t)
 
 # %%
-for file_name in sorted(glob.glob("text/*/wiki*")):
-    result = []
-    with open(file_name, encoding="utf-8") as f:
-        for line in f.readlines():
-            if line.startswith("<doc") or line.startswith("</doc"):
-                result.append("")
-            else:
-                result.append(remove_latin(line))
-    with open(file_name.replace("text","text/cleaned"), "w", encoding="utf-8") as f:
-        f.write("\n".join(result))
-    print(file_name)
+# for file_name in sorted(glob.glob("text/*/wiki*")):
+#     result = []
+#     with open(file_name, encoding="utf-8") as f:
+#         for line in f.readlines():
+#             if line.startswith("<doc") or line.startswith("</doc"):
+#                 result.append("")
+#             else:
+#                 result.append(remove_latin(line))
+#     with open(file_name.replace("text","text/cleaned"), "w", encoding="utf-8") as f:
+#         f.write("\n".join(result))
+#     print(file_name)
 #%%
-text = ""
-for file_name in sorted(glob.glob("text/cleaned/*/wiki*")):
-    with open(file_name, encoding="utf-8") as f:
-        text += f.read()
-save_text_as_gzip(text, "chinese_text.txt.gz")
-print("read",len(text),"characters")
+# text = ""
+# for file_name in sorted(glob.glob("text/cleaned/*/wiki*")):
+#     with open(file_name, encoding="utf-8") as f:
+#         text += f.read()
+# save_text_as_gzip(text, "chinese_text.txt.gz")
+# print("read",len(text),"characters")
+#%%
+text = load_text_from_gzip("chinese_text.txt.gz")
 # %%
 def dict_frequency(dictionary, top = 30):
     frequency_sorted = sorted(list(zip(list(dictionary.values()), zip(list(dictionary.keys())))), reverse=True)
@@ -55,6 +58,16 @@ def ngrams(n, ignore = ["\n", " ", "_", "：", "、", "\u3000", "\xa0", "\u200b"
 # %%
 frequency = ngrams(4)
 # %%
+def get_permutations(input_string):
+    # Use permutations to generate all possible permutations of the input string
+    perms = permutations(input_string)
+
+    # Convert each permutation tuple to a string and store in a list
+    perm_list = [''.join(p) for p in perms]
+
+    return list(set(perm_list))
+get_permutations("aa")
+# %%
 print("found", len(frequency), "ngrams")
 def common_reverse(frequency, min_frequency = 30):
     reverses = []
@@ -65,14 +78,32 @@ def common_reverse(frequency, min_frequency = 30):
                 reverses.append((min(frequency[key], frequency[reverse_key]), key))
     reverses_sorted = sorted(reverses, reverse=True)
     return(reverses_sorted)
+#%%
+def common_perms(frequency, min_frequency = 30):
+    permutations = []
+    for key in frequency:
+        if frequency[key] >= min_frequency:
+            key_permutations = get_permutations(key)
+            min_count = 100000000
+            for permutation in key_permutations:
+                if permutation not in frequency or permutation>key or frequency[permutation]<min_frequency:
+                    min_count = 0
+                    break
+                else:
+                    min_count = min(min_count,frequency[permutation])
+            if min_count>=min_frequency:
+                permutations.append((min_count, key))
+    sorted_perms = sorted(permutations, reverse = True)
+    return sorted_perms
+
 # %%
-reverses = common_reverse(frequency, 20)
-print("found", len(reverses), "reverses")
+perms = common_perms(frequency, 1)
+print("found", len(perms), "reverses")
 
 chars = dict()
 
-for count,reverse in reverses:
-    for char in reverse:
+for count,perm in perms:
+    for char in perm:
         if char not in chars:
             chars[char] = 1
         else:
