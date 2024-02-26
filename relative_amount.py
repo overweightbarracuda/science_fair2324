a45f71ea09f9b1c66a2b6a99a428fabcb96c23a2
Rioshanohan <rohanatl07@gmail.com>
2024-02-25 21:53:18 -0500

    relative in range

--- relative_amount.py
+++ relative_amount.py
@@ -26,8 +26,11 @@ def relative_amount(character, block):
 
 print(relative_amount("风", "自_年_陈国富的工作重心逐渐转入两岸3地华语电影的整合与开创_年之后_他与中国第1民营电影公司华谊兄弟合作_在_年中监制了_集结号_、_风声_、_唐山大地震_、_狄仁杰_系列、_太极_系列、_画皮_、_1942_等_部影片_其中_部票房过亿_至_年底_陈国富监制电影的累计票房已达到了_亿人民币_"))
 
+grepped_text = ""
 a = grep_all("1942")
-grepped_text = "".join(a[2])
+for i in range(1939, 1946):
+    a = grep_all(str(i))
+    grepped_text += "".join(a[2])
 
 
 
